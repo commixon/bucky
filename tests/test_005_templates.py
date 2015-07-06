550052a91bd59e6226e56652d8b106bfd586f924
Dimitris Rozakis <dimrozakis@gmail.com>
2015-07-06 22:51:52 +0300

    Add parser tests

--- tests/test_005_templates.py
+++ tests/test_005_templates.py
@@ -51,7 +51,7 @@ def test_filter_tree():
     t.eq(parser.filter_tree.get_tuple_tree(), tree)
 
 
-def test_apply_template():
+def test_template():
     cases = [
         {
             'test': 'metric only',
@@ -122,13 +122,91 @@ def test_apply_template():
         },
     ]
     for case in cases:
-        def _test_apply_template():
+        def _test_template():
             template = bucky.templates.Template(case['template'])
             measurement, tags = template.process(case['name'])
             t.eq(measurement, case['measurement'])
             t.eq(tags, case.get('tags', {}))
 
-        _test_apply_template.description = "Template test '%s': %s on %s" % (
+        _test_template.description = "Template test '%s': %s on %s" % (
             case['test'], case['template'], case['name']
         )
-        yield _test_apply_template
+        yield _test_template
+
+
+def test_parser():
+    cases = [
+        {
+            'test': 'match default',
+            'name': 'cpu',
+            'measurement': 'cpu',
+        },
+        {
+            'test': 'match multiple measurement',
+            'templates': 'servers.localhost.* .host.measurement.measurement*',
+            'name': 'servers.localhost.cpu.cpu_load.10',
+            'measurement': 'cpu.cpu_load.10',
+            'tags': {'host': 'localhost'},
+        },
+        {
+            'test': 'match multiple measurement with separator',
+            'separator': '_',
+            'templates': 'servers.localhost.* .host.measurement.measurement*',
+            'name': 'servers.localhost.cpu.cpu_load.10',
+            'measurement': 'cpu_cpu_load_10',
+            'tags': {'host': 'localhost'},
+        },
+        {
+            'test': 'match wildcard',
+            'templates': 'servers.* .host.measurement*',
+            'name': 'servers.localhost.cpu_load',
+            'measurement': 'cpu_load',
+            'tags': {'host': 'localhost'},
+        },
+        {
+            'test': 'match exact before wildcard',
+            'templates': [
+                'servers.* .wrong.measurement*',
+                'servers.localhost .host.measurement*',
+            ],
+            'name': 'servers.localhost.cpu_load',
+            'measurement': 'cpu_load',
+            'tags': {'host': 'localhost'},
+        },
+        {
+            'test': 'longest match first',
+            'templates': [
+                '*.* .wrong.measurement*',
+                'servers.* .wrong.measurement*',
+                'servers.localhost .wrong.measurement*',
+                'servers.localhost.cpu .host.resource.measurement*',
+                '*.localhost .wrong.measurement*'
+            ],
+            'name': 'servers.localhost.cpu.cpu_load',
+            'measurement': 'cpu_load',
+            'tags': {'host': 'localhost', 'resource': 'cpu'},
+        },
+        {
+            'test': 'match multiple wildcards',
+            'templates': [
+                '*.* .wrong.measurement*',
+                'servers.* .host.measurement*',
+                'servers.localhost .wrong.measurement*',
+                '*.localhost .wrong.measurement*',
+            ],
+            'name': 'servers.server01.cpu_load',
+            'measurement': 'cpu_load',
+            'tags': {'host': 'server01'},
+        },
+    ]
+    for case in cases:
+        def _test_parser():
+            parser = bucky.templates.NameParser(case.get('templates', ''),
+                                                case.get('separator', '.'),
+                                                case.get('global_tags'))
+            measurement, tags = parser.process(case['name'])
+            t.eq(measurement, case['measurement'])
+            t.eq(tags, case.get('tags', {}))
+
+        _test_parser.description = "Parser test '%s'." % case['test']
+        yield _test_parser
